4d310e793e4d1948c6ff0b3499907cf4af18e0bd
mr-rayt <robtechgroup@gmail.com>
2018-06-11 09:22:44 +0800

    initial-commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+// Express Middleware functions
+// This will stop everything from working since line is located above the other functions
+app.use((req, res, next) => {
+   res.render('maintenance.hbs');
+});
+
+app.get('/', (req, res) => {
+   // res.send('<h1>Hello from RAYT! </h1>');
+   res.send({
+      name: 'Robert Tabuan',
+      address: ' Aroroy, Masbate',
+      hobbies: [
+         'boxing', 'singing', 'kickboxing', 'table tennins', 'basketball', 'arnis', 'martial arts'
+      ]
+   });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about.hbs', {
+      pageTitle: 'About Page - rayt',
+      currentYear: new Date().getFullYear()
+   });
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+      errorMessage: 'Bad Request'
+   });
+});
+
+app.listen(3000, () => {
+   console.log('Server is up on port 3000...');
+});
